9476604296c52cd68601d0431af74add7312a564
Alexis Ramirez <alexisr1993@gmail.com>
2020-08-01 20:46:26 -0700

    Add routes to Admin

--- src/Admin/Admin.js
+++ src/Admin/Admin.js
@@ -3,15 +3,33 @@ import Container from 'react-bootstrap/Container';
 import AdminNav from './AdminNav';
 import QuizList from './QuizList';
 import ViewQuiz from './ViewQuiz';
+import Create from './Create';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function Admin() {
   return (
-    <>
+    <Router>
       <AdminNav/>
       <Container>
-        <QuizList/>
+      <Switch>
+        <Route path="/admin/home">
+          <>
+          </>
+        </Route>
+        <Route path="/admin/quizzes">
+          <QuizList/>
+        </Route>
+        <Route path="/admin/create">
+          <Create/>
+        </Route>
+    </Switch>
       </Container>
-    </>
+    </Router>
   );
 }
 
--- src/Admin/AdminNav.js
+++ src/Admin/AdminNav.js
@@ -3,22 +3,36 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function AdminNav() {
   return (
     <Row style={{backgroundColor: '#f8f9fa'}}>
       <Container>
         <Navbar bg="light" expand="md">
-          <Navbar.Brand href="#home">Quiz App</Navbar.Brand>
+          <Navbar.Brand>Quiz App</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
-              <Nav.Link href="#Home">Home</Nav.Link>
-              <Nav.Link href="#Quizzes">Quizzes</Nav.Link>
-              <Nav.Link href="#Create">Create</Nav.Link>
+              <Link to="/admin/home">
+                <Nav.Link href="#home">Home</Nav.Link>
+              </Link>
+              <Link to="/admin/quizzes">
+                <Nav.Link href="#quizzes">Quizzes</Nav.Link>
+              </Link>
+              <Link to="/admin/create">
+                <Nav.Link href="#create">Create</Nav.Link>
+              </Link>
             </Nav>
             <Nav>
-              <Nav.Link href="#Signout">Sign Out</Nav.Link>
+              <Link to="/admin/signout">
+                <Nav.Link href="#signout">Sign Out</Nav.Link>
+              </Link>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
