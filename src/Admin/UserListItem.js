7b80818acd0aa5ced3b2a727eb01f4ba729b093e
Alexis Ramirez <alexisr1993@gmail.com>
2020-08-12 15:26:36 -0700

    Add working update button

--- src/Admin/UserListItem.js
+++ src/Admin/UserListItem.js
@@ -6,6 +6,8 @@ import Table from 'react-bootstrap/Table';
 import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
 
+let url = 'http://localhost:4000';
+
 class UserList extends React.Component {
   constructor(props) {
     super(props);
@@ -17,6 +19,8 @@ class UserList extends React.Component {
 
     this.handleShow = this.handleShow.bind(this);
     this.handleClose = this.handleClose.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClickSaveChanges = this.handleClickSaveChanges.bind(this);
   }
 
   handleClose() {
@@ -24,6 +28,27 @@ class UserList extends React.Component {
   }
   handleShow() {
     this.setState({show: true});
+  }
+  handleChange(event) {
+    this.setState({[event.target.name]: event.target.value});
+  }
+  handleClickSaveChanges(event) {
+    fetch(url + '/user', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        "username": this.state.username,
+        "email": this.state.email
+      })
+    })
+      .then(res => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+     .catch(console.log)
+    event.preventDefault();
   }
   render() {
     return (
@@ -47,12 +72,24 @@ class UserList extends React.Component {
             <Form>
               <Form.Group controlId="formUsername">
                 <Form.Label>Username</Form.Label>
-                <Form.Control type="username" placeholder="Username"/>
+                <Form.Control
+                  name="username"
+                  type="username"
+                  placeholder="Username"
+                  value={this.state.username}
+                  onChange={this.handleChange}
+                />
               </Form.Group>
 
               <Form.Group controlId="formBasicEmail">
                 <Form.Label>Email</Form.Label>
-                <Form.Control type="email" placeholder="Email"/>
+                <Form.Control
+                  name="email"
+                  type="email"
+                  placeholder="Email"
+                  value={this.state.email}
+                  onChange={this.handleChange}
+                  />
               </Form.Group>
             </Form>
           </Modal.Body>
@@ -60,7 +97,7 @@ class UserList extends React.Component {
             <Button variant="outline-secondary" onClick={this.handleClose}>
               Close
             </Button>
-            <Button variant="outline-secondary" onClick={this.handleClose}>
+            <Button variant="outline-secondary" onClick={this.handleClickSaveChanges}>
               Save Changes
             </Button>
           </Modal.Footer>
