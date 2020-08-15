aad578874d916c3b7fd10ecdaf6954d6922f0577
Alexis Ramirez <alexisr1993@gmail.com>
2020-08-14 19:59:58 -0700

    Add question form

--- src/User/ViewQuiz.js
+++ src/User/ViewQuiz.js
@@ -8,6 +8,7 @@ import Row from 'react-bootstrap/Row';
 import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
 
 let url = 'http://localhost:4000';
 
@@ -17,6 +18,19 @@ function ViewQuiz(props) {
   const [quizDescription, setQuizDescription] = useState('');
   const [username, setUsername] = useState(localStorage.getItem('username'));
   const [quizQuestions, setQuizQuestions] = useState([]);
+  const [showQuestionModal, setShowQuestionModal] = useState(false);
+
+  const handleShowQuestionModal = () => {
+    setShowQuestionModal(true);
+  };
+
+  const handleCloseQuestionModal = () => {
+    setShowQuestionModal(false);
+  };
+
+  const handleSaveQuestion = () => {
+
+  };
 
   useEffect(() => {
     fetch(url + '/quiz/' + username + '/' + quizId, {
@@ -36,52 +50,147 @@ function ViewQuiz(props) {
   }, [quizId]);
 
   return (
-    <Container className="mt-4">
-      <Row>
-        <Card className="mb-4" style={{ width: '100%'}}>
-          <Card.Body>
-            <Card.Title>{quizTitle}</Card.Title>
-            <Card.Text>
-              {quizDescription}
-            </Card.Text>
-            <ButtonGroup className="float-right">
-              <Button variant="outline-secondary">Edit</Button>
-            </ButtonGroup>
-          </Card.Body>
-        </Card>
-      </Row>
-      <Row className="mb-4">
-        <ButtonGroup className="float-right">
-          <Button variant="outline-secondary">Add Question</Button>
-          <Button variant="outline-secondary">Select All</Button>
-        </ButtonGroup>
-      </Row>
-      <Row>
-        <ListGroup.Item className="text-left" style={{width: '100%'}}>
-          <Nav.Item>
-            <Row>
-              <Col className="d-flex align-items-center" md={1}>
-                <Form.Check type="checkbox" label="" />
-              </Col>
-              <Col md={9}>
-                <h6>
-                  Question 1
-                </h6>
-                <p>
-                  This is a question?
-                </p>
-              </Col>
-              <Col md={2}>
-                <ButtonGroup className="float-right">
-                  <Button variant="outline-secondary">Edit</Button>
-                  <Button variant="outline-secondary">Delete</Button>
-                </ButtonGroup>
-              </Col>
-            </Row>
-          </Nav.Item>
-        </ListGroup.Item>
-      </Row>
-    </Container>
+    <>
+      <Container className="mt-4">
+        <Row>
+          <Card className="mb-4" style={{ width: '100%'}}>
+            <Card.Body>
+              <Card.Title>{quizTitle}</Card.Title>
+              <Card.Text>
+                {quizDescription}
+              </Card.Text>
+              <ButtonGroup className="float-right">
+                <Button variant="outline-secondary">Edit</Button>
+              </ButtonGroup>
+            </Card.Body>
+          </Card>
+        </Row>
+        <Row className="mb-4">
+          <ButtonGroup className="float-right">
+            <Button variant="outline-secondary" onClick={handleShowQuestionModal}>Add Question</Button>
+            <Button variant="outline-secondary">Select All</Button>
+          </ButtonGroup>
+        </Row>
+        <Row>
+          <ListGroup.Item className="text-left" style={{width: '100%'}}>
+            <Nav.Item>
+              <Row>
+                <Col className="d-flex align-items-center" md={1}>
+                  <Form.Check type="checkbox" label="" />
+                </Col>
+                <Col md={9}>
+                  <h6>
+                    Question 1
+                  </h6>
+                  <p>
+                    This is a question?
+                  </p>
+                </Col>
+                <Col md={2}>
+                  <ButtonGroup className="float-right">
+                    <Button variant="outline-secondary">Edit</Button>
+                    <Button variant="outline-secondary">Delete</Button>
+                  </ButtonGroup>
+                </Col>
+              </Row>
+            </Nav.Item>
+          </ListGroup.Item>
+        </Row>
+      </Container>
+
+      <Modal show={showQuestionModal}>
+        <Modal.Header closeButton onClick={handleCloseQuestionModal}>
+          <Modal.Title>Add New Question</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="formUsername">
+              <Form.Label>Question</Form.Label>
+              <Form.Control
+                as="textarea"
+                name="question"
+                placeholder="Question"
+              />
+            </Form.Group>
+            <Form.Group controlId="formUsername">
+              <Form.Label>Answer 1</Form.Label>
+              <Form.Control
+                name="answer1"
+                placeholder="Answer 1"
+              />
+            </Form.Group>
+            <Form.Group controlId="formUsername">
+              <Form.Label>Answer 2</Form.Label>
+              <Form.Control
+                name="answer2"
+                placeholder="Answer 2"
+              />
+            </Form.Group>
+            <Form.Group controlId="formUsername">
+              <Form.Label>Answer 3</Form.Label>
+              <Form.Control
+                name="answer3"
+                placeholder="Answer 3"
+              />
+            </Form.Group>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Answer 4</Form.Label>
+              <Form.Control
+                name="title"
+                placeholder="Answer 4"
+                />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label as="legend">
+                Select correct answer
+              </Form.Label>
+              <Row>
+                <Col>
+                  <Form.Check
+                    type="radio"
+                    label="Answer 1 "
+                    name="answer"
+                    id="formHorizontalRadios1"
+                  />
+                </Col>
+                <Col>
+                  <Form.Check
+                    type="radio"
+                    label="Answer 2"
+                    name="answer"
+                    id="formHorizontalRadios2"
+                  />
+                </Col>
+                <Col>
+                  <Form.Check
+                    type="radio"
+                    label="Answer 3"
+                    name="answer"
+                    id="formHorizontalRadios3"
+                  />
+                </Col>
+                <Col>
+                  <Form.Check
+                    type="radio"
+                    label="Answer 5"
+                    name="answer"
+                    id="formHorizontalRadios3"
+                  />
+                </Col>
+              </Row>
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="outline-secondary" onClick={handleCloseQuestionModal}>
+            Close
+          </Button>
+          <Button variant="outline-secondary" onClick={handleSaveQuestion}>
+            Save Question
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 }
 
