a765c8eb5cb985909e68c4d93e9121127cfb5322
Alexis Ramirez <alexisr1993@gmail.com>
2020-09-18 16:01:41 -0700

    Fix user dashboard

--- src/User/UserDashboard.js
+++ src/User/UserDashboard.js
@@ -1,18 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import Card from 'react-bootstrap/Card';
 
 function UserDashboard() {
+  const [username, setUserName] = useState(localStorage.getItem('username'));
+  const [resultList, setResultList] = useState([]);
+  const [dashboardData, setDashBoardData] = useState({
+    quizzesTaken: 0,
+    lastQuizTaken: 'None',
+    quizzesFailed: 0,
+  })
+
+  useEffect(() => {
+    fetch(process.env.REACT_APP_API_URL + '/results/' + username, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    })
+    .then(res => res.json())
+    .then((data) => {
+      let failed = data.filter(element => {
+        return parseInt(element.quizScore.slice(0 , -1)) < 60;
+      }).length;
+      
+      setDashBoardData({
+        quizzesTaken: data.length,
+        lastQuizTaken: data[Object.keys(data)[Object.keys(data).length - 1]].quizTitle ,
+        quizzesFailed: failed,
+      });
+    })
+    .catch(console.log)
+  }, []);
   return (
     <>
-      <h2 className="mt-4 mb-5">Hi, {localStorage.getItem('username')}!</h2>
+      <h2 className="mt-4 mb-5">Hi, {username}!</h2>
       <h4>Quizzes Take:</h4>
-      <h1>10</h1>
+      <h1>{dashboardData.quizzesTaken}</h1>
       <br/>
       <h4>Last Quiz Taken:</h4>
-      <h1>Quiz 1</h1>
+      <h1>{dashboardData.lastQuizTaken}</h1>
       <br/>
-      <h4>Quizzes Failed</h4>
-      <h1>5</h1>
+      <h4>Quizzes Failed:</h4>
+      <h1>{dashboardData.quizzesFailed}</h1>
     </>
   )
 }
